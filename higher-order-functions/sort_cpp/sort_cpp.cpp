0acea47e1450952d78ce5d02bbdd7b6e9fdb65f4
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-12-22 12:49:12 +0530

    Update sort program to support --length.

--- higher-order-functions/sort_cpp/sort_cpp.cpp
+++ higher-order-functions/sort_cpp/sort_cpp.cpp
@@ -11,11 +11,25 @@ using namespace std;
 // - If the first argument is "--alphabet", the remaining strings shall be sorted alphabetically.
 // - If the first argument does not start with "--", the strings shall be sorted alphabetically.
 
+auto by_alphabet = [](const string &w1, const string &w2) { return w1 < w2; };
+auto by_length =   [](const string &w1, const string &w2) { return w1.length() < w2.length(); };
+
+// The variable by_comparator is the strategy design pattern.
+// It allows switching between different algorithms ("strategies") at runtime.
+// The two strategies available in this program for the by_comparator are by_alphabet and by_length.
+
 int main(int argc, char *argv[]) {
+    bool useLength = false;
     vector<string> words;
+    if (argc > 1 && string(argv[1]).rfind("--", 0) == 0) {
+        if (string(argv[1]) == "--length")
+            useLength = true;
+        ++argv;
+    }
     while (*++argv)
         words.push_back(*argv);
-    sort(words.begin(), words.end(), [](const string &w1, const string &w2) { return w1 < w2; } );
+    auto by_comparator = useLength ? by_length : by_alphabet;
+    sort(words.begin(), words.end(), by_comparator);
     for (auto word : words)
         cout << word << endl;
 }
