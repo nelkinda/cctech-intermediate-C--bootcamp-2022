b42db30d8f22c74e9288c2d245c1ad68519a8206
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-12-23 09:46:35 +0530

    Improve code to make better use of lambdas.

--- higher-order-functions/sort_cpp/sort_cpp.cpp
+++ higher-order-functions/sort_cpp/sort_cpp.cpp
@@ -11,25 +11,27 @@ using namespace std;
 // - If the first argument is "--alphabet", the remaining strings shall be sorted alphabetically.
 // - If the first argument does not start with "--", the strings shall be sorted alphabetically.
 
-auto by_alphabet = [](const string &w1, const string &w2) { return w1 < w2; };
-auto by_length =   [](const string &w1, const string &w2) { return w1.length() < w2.length(); };
+//bool by_alphabet(const string &w1, const string &w2) { return w1 < w2; };
+//bool by_length(const string &w1, const string &w2) { return w1.length() < w2.length(); };
 
 // The variable by_comparator is the strategy design pattern.
 // It allows switching between different algorithms ("strategies") at runtime.
 // The two strategies available in this program for the by_comparator are by_alphabet and by_length.
 
 int main(int argc, char *argv[]) {
-    bool useLength = false;
+    auto by_alphabet = [](const string &w1, const string &w2) { return w1 < w2; };
+    auto by_length =   [](const string &w1, const string &w2) { return w1.length() < w2.length(); };
+
+    bool (*comparator)(const string &, const string &) = by_alphabet;
     vector<string> words;
     if (argc > 1 && string(argv[1]).rfind("--", 0) == 0) {
         if (string(argv[1]) == "--length")
-            useLength = true;
+            comparator = by_length;
         ++argv;
     }
     while (*++argv)
         words.push_back(*argv);
-    auto by_comparator = useLength ? by_length : by_alphabet;
-    sort(words.begin(), words.end(), by_comparator);
+    sort(words.begin(), words.end(), comparator);
     for (auto word : words)
         cout << word << endl;
 }
