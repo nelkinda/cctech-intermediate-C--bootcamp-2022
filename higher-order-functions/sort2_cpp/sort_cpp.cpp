bf857fe278265d3401e3249df6d259df8959525e
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-12-23 10:45:48 +0530

    Make it more OO.

--- higher-order-functions/sort2_cpp/sort_cpp.cpp
+++ higher-order-functions/sort2_cpp/sort_cpp.cpp
@@ -5,39 +5,43 @@
 
 using namespace std;
 
-auto by_alphabet = [](const string &s1, const string &s2) { return s1 < s2; };
-auto by_length =   [](const string &s1, const string &s2) { return s1.length() < s2.length(); };
+class Sorter {
+private:
+    static bool by_alphabet(const string &s1, const string &s2) { return s1 < s2; };
+    static bool by_length  (const string &s1, const string &s2) { return s1.length() < s2.length(); };
 
-bool (*comparator)(const string &, const string &) = by_alphabet;
-
-vector<string> readLines() {
     vector<string> lines;
-    for (string line; getline(cin, line);)
-        lines.push_back(line);
-    return lines;
-}
+    bool (*comparator)(const string &, const string &) = by_alphabet;
 
-void sortLines(vector<string> &lines) {
-    sort(lines.begin(), lines.end(), comparator);
-}
-
-void printLines(vector<string> &lines) {
-    for (auto word : lines)
-        cout << word << endl;
-}
-
-void parseCommandLineArguments(int argc, char *argv[]) {
-    if (argc > 1 && string(argv[1]).rfind("--", 0) == 0) {
-        if (string(argv[1]) == "--length")
-            comparator = by_length;
-        ++argv;
+public:
+    void parseCommandLineArguments(int argc, char *argv[]) {
+        if (argc > 1 && string(argv[1]).rfind("--", 0) == 0) {
+            if (string(argv[1]) == "--length")
+                comparator = by_length;
+            ++argv;
+        }
     }
-}
+
+    void readLines() {
+        for (string line; getline(cin, line);)
+            lines.push_back(line);
+    }
+
+    void sortLines() {
+        sort(lines.begin(), lines.end(), comparator);
+    }
+
+    void printLines() {
+        for (auto word : lines)
+            cout << word << endl;
+    }
+};
 
 int main(int argc, char *argv[]) {
-    parseCommandLineArguments(argc, argv);
+    Sorter sorter;
+    sorter.parseCommandLineArguments(argc, argv);
 
-    vector<string> lines = readLines();
-    sortLines(lines);
-    printLines(lines);
+    sorter.readLines();
+    sorter.sortLines();
+    sorter.printLines();
 }
