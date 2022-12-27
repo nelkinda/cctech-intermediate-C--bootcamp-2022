799042040e95ae3abefd15a5e8a915eeeb03d4c0
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-12-27 09:27:18 +0530

    Fix compilation issue.

--- regex/isValidTime/isValidTime.cpp
+++ regex/isValidTime/isValidTime.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    // Solution A
-    string hours = "([01][0-9]|2[0-3])";
-    string minutesOrSeconds = "([0-5][0-9])";
-    regex timePattern(hours + "(" + ":" + minutesOrSeconds + "){1,2}");
+    //// Solution A
+    //string hours = "([01][0-9]|2[0-3])";
+    //string minutesOrSeconds = "([0-5][0-9])";
+    //regex timePattern(hours + "(" + ":" + minutesOrSeconds + "){1,2}");
 
-    // Solution B
+    // Solution B (preferred)
     string hours = "([01][0-9]|2[0-3])";
     string minutes = "([0-5][0-9])";
     string seconds = "([0-5][0-9])";
